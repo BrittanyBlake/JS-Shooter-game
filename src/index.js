31d922150ff9dafb3ca004d0beeb5a6a37568647
Brittany Blake <brittany.blake49@gmail.com>
2020-09-15 13:54:25 +0200

    start styling game scene

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import Phaser from "phaser";
+import "./styles/style.css";
+import GameScene from './Scenes/GameScene';
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: [GameScene],
+};
+
+export default new Phaser.Game(config);
