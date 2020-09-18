1da7cd4f401550b5671d10785eeff9f6a2a2c8d6
Brittany Blake <brittany.blake49@gmail.com>
2020-09-18 20:29:03 +0200

    add tests for player attack

---
+++ test/playerAttackGroup.test.js
@@ -0,0 +1,5 @@
+import playerAttackGroup from "../src/helper/playerAttackGroup";
+
+test("player Attack Group is a subclass of Phaser.Scene", () => {
+  expect(playerAttackGroup).toBeSubclassOf(Phaser.Physics.Arcade.Group);
+});
